c7c809e274c3623d75efb552934dbcf62f35b733
hanguang254 <725204548@qq.com>
2025-07-21 17:04:54 +0800

    renew

--- TestCase/test_add.py
+++ TestCase/test_add.py
@@ -5,12 +5,14 @@ from common.Logger import Log
 
 class Test_add:
 
+    @classmethod
     def setup_class(self):
         file = os.path.basename(__file__)
         log = Log(file)
         self.logger = log.Logger()  # ✅ 用 self 没问题
         self.logger.info("setup_class called")
 
+    @classmethod
     def teardown_class(self):
         self.logger.info("teardown_class called")
 
